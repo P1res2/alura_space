c09dd033aeac205521de68e9732bbdce881b89b2
P1res2 <pires17.gg@gmail.com>
2025-07-02 03:28:51 -0300

    Criando-forms.py

---
+++ usuarios/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+
+class LoginForms(forms.Form):
+    nome_login = forms.CharField(
+        label='Nome de login',
+        required=True,
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex: Gabriel Silva',
+            }
+        )
+    )
+    senha = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            }
+        )
+    )
+    
+class CadastroForms(forms.Form):
+    nome_completo = forms.CharField(
+        label='Nome Completo',
+        required=True,
+        max_length=120,
+        widget=forms.TextInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex: Gabriel Pires Araujo',
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        widget=forms.EmailInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Ex: seuemail@exmp.com',
+            }
+        )
+    )
+    senha1 = forms.CharField(
+        label='Senha',
+        required=True,
+        max_length=70,
+        widget=forms.PasswordInput(
+            attrs={
+                'class':'form-control',
+                'placeholder':'Digite sua senha',
+            }
+        )
+    )
